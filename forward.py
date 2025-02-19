296034b39830a83b3c59103e5f6e2e074d088635
Karthik C V <147153484+karthikcv007@users.noreply.github.com>
2025-02-19 14:50:02 +0530

    forward.py

--- forward.py
+++ forward.py
@@ -7,6 +7,6 @@ def forward_difference(f, x, h=1e-5):
     return (f(x + h) - f(x)) / h
 
 # User input
-x_val = float(input("Enter the value of x: "))
+x_val = 2
 derivative = forward_difference(f, x_val)
 print(f"Approximate derivative at x={x_val}: {derivative}")
